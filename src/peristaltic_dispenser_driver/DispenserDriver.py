74e0745da3141327a8fefe8cdbb108a9f51a5297
Yak-28 <jakubglowacki@mail.com>
2021-08-02 16:00:58 +0100

    Changed PID control method, commented code

--- src/peristaltic_dispenser_driver/DispenserDriver.py
+++ src/peristaltic_dispenser_driver/DispenserDriver.py
@@ -1,30 +1,36 @@
-from gpiozero import LED
+#!/usr/bin/env python
+
+#Hardware driver for stepper motor based peristaltic pump
+#Utilizes an MCP4725 DAC for speed control, as well as a relay for switching
+#Made by Jakub Glowacki 02/08/2021
+
+from gpiozero import LED #controls GPIO
 import board
 import busio
-import adafruit_mcp4725
+import adafruit_mcp4725 #controls adafuit DAC
 import time
 
 class DispenserDriver:
-        global direct
+        global direct #Direction of pump boolean, default is false, used to reverse pump
         direct = False
         def __init__(self):
             global dac
             global stepper
             global direction
-            i2c = busio.I2C(board.SCL, board.SDA)
+            i2c = busio.I2C(board.SCL, board.SDA) #Create I2C connection with DAC
             dac = adafruit_mcp4725.MCP4725(i2c, address=0x60)
-            dac.value = 0
-            stepper = LED(17)
+            dac.value = 0 #DAC value set to 0 so pump is not running
+            stepper = LED(17) #Connect to both GPIO pins needed for pump control
             direction = LED(27)
-            stepper.on()
+            stepper.on() #on=off due to relay wiring, turn pump off by default
             direction.off()
             direct=False
             
-        def dispenserOn(self):
+        def dispenserOn(self): #Simply turn pump on
             global stepper
             stepper.off()
         
-        def reset(self):
+        def reset(self): #Set speed back to 0, direction to default off and turn pump off
             global stepper
             global direction
             global dac
@@ -32,7 +38,7 @@ class DispenserDriver:
             direction.off()
             dac.value=0
             
-        def reverseDirection(self):
+        def reverseDirection(self): #Reverse the direction of the pump
             global direction
             global direct
             if (direct):
@@ -42,11 +48,12 @@ class DispenserDriver:
                 direction.on()
                 direct=True
         
-        def dispenserOff(self):
+        def dispenserOff(self): #Turn off pump
             global stepper
             stepper.on()
             
-        def dispenseIndef(self, speed):
+        def dispenseIndef(self, speed): #Turn pump on at normalized speed (0 to 1).
+                                        #Will keep running until turned off
             global stepper
             global dac
             global direction
@@ -55,7 +62,7 @@ class DispenserDriver:
             direct=False
             dac.normalized_value=speed
         
-        def dispenseIndefReverse(self, speed):
+        def dispenseIndefReverse(self, speed): #Same as above but in other direction
             global stepper
             global dac
             global direction
@@ -64,7 +71,7 @@ class DispenserDriver:
             direct=True
             dac.normalized_value=speed
         
-        def dispense(self, speed, dispTime):
+        def dispense(self, speed, dispTime): #Turn pump on for specified amount of time
             global stepper
             global dac
             global direction
@@ -76,7 +83,7 @@ class DispenserDriver:
             stepper.on()
             dac.normalized_value=0
             
-        def dispenseReverse(self, speed, dispTime):
+        def dispenseReverse(self, speed, dispTime): #Same as above but in reverse
             global stepper
             global dac
             global direction
--- src/peristaltic_dispenser_driver/DispenserDriverROS.py
+++ src/peristaltic_dispenser_driver/DispenserDriverROS.py
@@ -1,48 +1,65 @@
+#!/usr/bin/env python
+
+#ROS Wrapper for a hardware driver for stepper motor based peristaltic pump
+#Also launches ROS Kern balance driver, as this is utilized for PID controlled liquid dispensing
+#Made by Jakub Glowacki 02/08/2021
+
 import rospy
-import roslaunch
-import math
+import roslaunch #launches other required package
+import math #Used for weight tolerance
 import time
-from kern_pcb_balance.msg import KernReading
+from kern_pcb_balance.msg import KernReading #weight reading from balance
 from peristaltic_dispenser_driver.DispenserDriver import DispenserDriver
 from peristaltic_dispenser_driver.msg import DispenserCommand
-from simple_pid import PID
+from simple_pid import PID #PID control library
 class DispDriverROS:
     def __init__(self):
         global weight
-        weight = 0.0
-        self.dispenser = DispenserDriver()
+        weight = 0.0 #initialize global weight from balance
+        self.dispenser = DispenserDriver() #Create instance of main driver
         rospy.Subscriber("Dispenser_Commands", DispenserCommand, self.callback_commands)
         rospy.Subscriber("/Kern_Weights", KernReading, self.weightCallback)
         rospy.loginfo("Dispenser Driver Started")
-        launch = roslaunch.scriptapi.ROSLaunch()
+        launch = roslaunch.scriptapi.ROSLaunch() #use roslaunch to launch balance driver
         launch.start()
         kernProcess = launch.launch(roslaunch.core.Node('kern_pcb_balance', 'KernPCBROS'))
         rospy.loginfo("Balance Process Launched")
-        
+    
+    #PID Dispensing function, asks for specified target weight of liquid
+    #to be dispensed
     def dispenseLiquid(self, liquidAmount):
         global weight
-        startTime = time.time()
-        pid = PID(0.1,0.5,0.01, setpoint=liquidAmount)
-        pid.sample_time = 0.2
-        pid.output_limits = (0,1)
+        startTime = time.time() #Start time defined for timeout
+        pid = PID(60, 0.3 ,0, setpoint=liquidAmount) #Begins PID control
+        pid.sample_time = 0.2 #Sample time for PID set to balance polling period
+        pid.output_limits = (0,1) #Limit output to range of DAC inputs, 0 to 1 as normalized
         targetReached = False
-        while (not targetReached):
+        while (not targetReached): #repeat until target weight reached
             output = pid(weight)
-            self.dispenseIndefinitely(output)
+            self.dispenseIndefinitely(output) #use PID process to adjust pump
+            #speed in order to dispense correct amount of liquid
+            
+            #Terminate process successfully if weight is within tolerance
             if (math.isclose(weight, liquidAmount, abs_tol=(0.05))):
                 self.reset()
                 rospy.loginfo("Liquid dispensed correctly within 0.05g tolerance!")
                 targetReached = True
+            #Terminate process unsucessfully if weight goes above tolerance,
+            #overshoot can't be fixed when dispensing liquid
             if (weight > liquidAmount + 0.05):
                 self.reset()
                 rospy.loginfo("Overshoot Error! Liquid overshot out of tolerance, check PID tuning")
                 targetReached = True
-            if (time.time() > startTime + 30):
+            #Terminate process unsucessfully if target not reached in 60 seconds
+            #this could indicate undershoot or another failure
+            if (time.time() > startTime + 60):
                 self.reset()
-                rospy.loginfo("Target not reached in over 30 seconds, check liquid, possible undershoot.")
+                rospy.loginfo("Target not reached in over 60 seconds, check liquid, possible undershoot.")
                 targetReached = True
         return True
         
+    #Below functions simply switch to the correct functions within
+    #the hardware driver as a response to a ROS command
     def on(self):
         self.dispenser.dispenserOn()
         rospy.loginfo("Turning on dispenser")
@@ -75,10 +92,12 @@ class DispDriverROS:
         self.dispenser.dispendseReverse(speed, dispTime)
         rospy.loginfo("Dispensing in Reverse for " + str(dispTime) + " seconds at Speed: " + str(speed))
     
+    #callback from receiving weight from balance. Updates corresponding variable.
     def weightCallback(self,msg):
         global weight
         weight = msg.weight
-        
+     
+    #Callback for receiving commands. Calls the appropriate function for each command.
     def callback_commands(self,msg):
         if(msg.dispenser_command == msg.ON):
             self.on()
